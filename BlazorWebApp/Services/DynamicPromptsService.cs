fbd1ceec8fb215c0b88f9f4a5550ff28c6cda3fe
Rogerooo <hugo.lopesmatias@gmail.com>
2023-03-22 15:41:25 +0000

    Import Python.Net

---
+++ BlazorWebApp/Services/DynamicPromptsService.cs
@@ -0,0 +1,29 @@
+﻿using Python.Runtime;
+
+namespace BlazorWebApp.Services
+{
+    public class DynamicPromptsService
+    {
+        private readonly IConfiguration _configuration;
+
+        public DynamicPromptsService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", _configuration["PythonPath"]);
+            PythonEngine.Initialize();
+            PythonEngine.BeginAllowThreads();
+            Test();
+        }
+
+        public void Test()
+        {
+            int dotnetNumber = 3;
+            using var _ = Py.GIL();
+            using var scope = Py.CreateScope();
+            scope.Set("pythonNumber", dotnetNumber);
+            scope.Exec("calculation = pythonNumber * 2");
+            dynamic calc = scope.Get("calculation");
+            Console.WriteLine("Calculation from Python: " + calc);
+        }
+    }
+}
