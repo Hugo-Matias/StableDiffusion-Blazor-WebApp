ef7829fd574568579346dae0d9629f6d6b247f12
Rogerooo <hugo.lopesmatias@gmail.com>
2023-04-17 17:45:30 +0100

    Update ControlNet preprocessors

--- BlazorWebApp/Models/ScriptParametersControlNet.cs
+++ BlazorWebApp/Models/ScriptParametersControlNet.cs
@@ -19,5 +19,5 @@ namespace BlazorWebApp.Models
         [JsonPropertyName("guidance_end")] public float GuidanceEnd { get; set; }
         [JsonPropertyName("guessmode")] public bool IsGuessMode { get; set; }
     }
-    public enum ControlNetPreprocessor { none, canny, depth, depth_leres, hed, mlsd, normal_map, openpose, openpose_hand, clip_vision, color, pidinet, scribble, fake_scrible, segmentation, binary }
+    public enum ControlNetPreprocessor { none, canny, depth_leres, depth_midas, depth_zoe, inpaint_global_harmonious, lineart, lineart_anime, lineart_coarse, mlsd, normal_bae, normal_midas, openpose, openpose_face, openpose_faceonly, openpose_full, openpose_hand, scribble_hed, scribble_pidinet, scribble_xdog, seg_ofade20k, seg_ofcoco, seg_ufade20k, shuffle, softedge_hed, softedge_hedsafe, softedge_pidinet, softedge_pidisafe, t2ia_color_grid, t2ia_sketch_pidi, t2ia_style_clipvision, threshold, tile_gaussian }
 }
